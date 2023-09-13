105f5da6342e5d98223570def5d913f83e09d2b9
ahmedelkady513 <84418660+ahmedelkady513@users.noreply.github.com>
2023-09-13 21:57:23 +0300

    first commit

---
+++ src/test/java/com/elkady/aopdemo/AopdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elkady.aopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
