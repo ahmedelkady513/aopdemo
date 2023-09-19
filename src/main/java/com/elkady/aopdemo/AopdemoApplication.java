bf09eff47c9e6b7d02d575f9e90d4ad7a6e9b2f8
ahmedelkady513 <84418660+ahmedelkady513@users.noreply.github.com>
2023-09-19 21:33:31 +0300

    Before Advice

--- src/main/java/com/elkady/aopdemo/AopdemoApplication.java
+++ src/main/java/com/elkady/aopdemo/AopdemoApplication.java
@@ -1,7 +1,11 @@
 package com.elkady.aopdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.elkady.aopdemo.dao.AccountDAO;
 
 @SpringBootApplication
 public class AopdemoApplication {
@@ -10,4 +14,17 @@ public class AopdemoApplication {
 		SpringApplication.run(AopdemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO) {
+		return runner -> {
+			demoTheBeforeAdvice(theAccountDAO);
+		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO) {
+		theAccountDAO.addAccount();
+
+		theAccountDAO.addAccount();
+
+	}
 }
---
+++ src/main/java/com/elkady/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,6 @@
+package com.elkady.aopdemo.dao;
+
+public interface AccountDAO {
+    
+    void addAccount();
+}
