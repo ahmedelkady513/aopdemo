80ffc4c5dec2f3ce08a873f1606385c9871cabf8
ahmedelkady513 <84418660+ahmedelkady513@users.noreply.github.com>
2023-09-22 11:04:13 +0300

    multiple Usage of point cut expression

--- src/main/java/com/elkady/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/elkady/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,14 +2,19 @@ package com.elkady.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
     
-    @Before("execution(public void addAccount())")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n=====>>> Executing @Before advice on addAccount()");
+    @Pointcut("execution(public void addAccount())")
+    public void forAddAccountMethod() {
+    }
+    
+    @Before("forAddAccountMethod")
+    public void  beforeAddAccountAdvice() {
+        System.out.println("===> bfore add account");
     }
 }
